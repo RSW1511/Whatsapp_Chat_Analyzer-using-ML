3cf1d2a9ffc5cd5289f6f7c9c7681e3f7429c477
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-03-31 23:46:11 +0530

    created a function for checking most common words

--- helper.py
+++ helper.py
@@ -47,3 +47,27 @@ def create_wordcloud(selected_user,df):
     temp['message'] = temp['message'].apply(remove_stop_words)
     df_wc = wc.generate(temp['message'].str.cat(sep=" "))
     return df_wc
+
+def most_common_words(selected_user,df):
+
+    f = open('stop_hinglish.txt','r')
+    stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    words = []
+
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
+
+
+    
