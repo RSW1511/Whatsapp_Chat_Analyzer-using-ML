529bb6ba3b05d524c0b6ed8b84dd1babd7d26109
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-04-01 00:13:32 +0530

    Commented the period

--- preprocessor.py
+++ preprocessor.py
@@ -37,7 +37,8 @@ def preprocess(data):
     #df['day_name'] = df['date'].dt.day_name()
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
-
+    return df
+"""
     period = []
     for hour in df[['day_name', 'hour']]['hour']:
         if hour == 23:
@@ -48,4 +49,4 @@ def preprocess(data):
             period.append(str(hour) + "-" + str(hour + 1))
 
     df['period'] = period
-    return df
+"""
