4ee46d0d2f47cda841b6019c3728ae37770a8e6a
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-03-31 22:44:23 +0530

    Created a helper.py for visulaizing

---
+++ helper.py
@@ -0,0 +1,20 @@
+from urlextract import URLExtract
+from wordcloud import WordCloud
+import pandas as pd
+from collections import Counter
+import emoji
+
+extract = URLExtract()
+
+def fetch_stats(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    # fetch the number of messages
+    num_messages = df.shape[0]
+
+    # fetch the total number of words
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
