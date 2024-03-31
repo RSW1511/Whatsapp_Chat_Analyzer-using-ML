1297100c47ed5c2621c94b3067a79921257b812e
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-03-31 23:31:05 +0530

    created a function for crearing worldcloud

--- helper.py
+++ helper.py
@@ -24,3 +24,14 @@ def most_busy_users(df):
     df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
         columns={'index': 'name', 'user': 'percent'})
     return x,df
+
+def create_wordcloud(selected_user,df):
+
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
