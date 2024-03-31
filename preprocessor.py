bf7c2d9e277befe98adb9b663b9edfe7184d814c
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-03-31 21:51:15 +0530

    Added preprocess ml model

---
+++ preprocessor.py
@@ -0,0 +1,27 @@
+import re
+import pandas as pd
+
+
+def preprocess(data):
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    # convert message_date type
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:  # user name
+            users.append(entry[1])
+            messages.append([2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
