507e0705133e6e3d6abb6472566ab87105e92eea
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-03-31 22:24:37 +0530

    File uploader in streamlit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import preprocessor
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
+    st.dataframe(df)
