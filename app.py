7cea4165b6a8250c52383638d4a7860022538d39
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-03-31 22:27:04 +0530

    Added unique users for txt

--- app.py
+++ app.py
@@ -11,3 +11,11 @@ if uploaded_file is not None:
     data = bytes_data.decode("utf-8")
     df = preprocessor.preprocess(data)
     st.dataframe(df)
+
+ # fetch unique users
+    user_list = df['user'].unique().tolist()
+    #user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,"Overall")
+
+    selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
