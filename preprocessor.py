1122e7028c9a16c968adb41cc1710346c221d7ce
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-03-31 22:14:30 +0530

    Added a new app

--- preprocessor.py
+++ preprocessor.py
@@ -25,3 +25,27 @@ def preprocess(data):
             users.append('group_notification')
             messages.append(entry[0])
 
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    period = []
+    for hour in df[['day_name', 'hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour == 0:
+            period.append(str('00') + "-" + str(hour + 1))
+        else:
+            period.append(str(hour) + "-" + str(hour + 1))
+
+    df['period'] = period
+    return df
