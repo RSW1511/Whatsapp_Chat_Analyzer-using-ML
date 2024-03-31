37bdb5b69aa0e847fe76825b1a6fe5b1896d8279
RSW1511 <rohanwaghode410@apsit.edu.in>
2024-03-31 23:54:06 +0530

    created a function for week activity map

--- helper.py
+++ helper.py
@@ -104,7 +104,15 @@ def daily_timeline(selected_user,df):
     daily_timeline = df.groupby('only_date').count()['message'].reset_index()
 
     return daily_timeline
-    
+
+def week_activity_map(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    return df['day_name'].value_counts()
+
+
 def month_activity_map(selected_user,df):
 
     if selected_user != 'Overall':
